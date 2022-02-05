aa8b9c093c3ff5e41ec64e2ca1c91aae9cbac2d3
Guilherme <guilherme@nfe.io>
2022-02-05 18:29:52 -0300

    implement active button style

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,8 +1,29 @@
-import { ButtonHTMLAttributes } from "react";
-import './styles.scss';
+import classNames from "classnames";
+import { ButtonHTMLAttributes, ReactNode } from "react";
 
-export function Button({ ...props }: ButtonHTMLAttributes<HTMLButtonElement>) {
+import './styles.scss';
+import closeIcon from '../../assets/images/close-icon.svg';
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  active?: boolean;
+  children?: ReactNode;
+};
+
+export function Button({ active, children,  ...props }: ButtonProps) {
   return (
-    <button className="button" {...props} />
+    <button className={classNames(
+        'button',
+        {active}
+      )}
+      {...props}
+    >
+      {children}
+
+      {active && (
+        <div className="button__close">
+          <img src={closeIcon} alt="botão de remover filtro" />
+        </div>  
+      )}
+    </button>
   )
 }

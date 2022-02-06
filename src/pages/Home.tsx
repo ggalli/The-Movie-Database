16b601fd46d6893580fa9a31a27ceb11a55b9c90
Guilherme <guilherme@nfe.io>
2022-02-06 18:37:19 -0300

    refactor filter and implement pagination

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import { useState, useEffect } from "react";
-import { useSearchParams, useNavigate } from "react-router-dom";
+import { useSearchParams, useNavigate, createSearchParams } from "react-router-dom";
+import ReactPaginate from 'react-paginate';
+
 import { Button } from "../components/Button";
 import { MovieCard } from "../components/MovieCard";
-import "../styles/home.scss";
-
 import { api } from "../services/api";
+
+import "../styles/home.scss";
 
 type GenreProps = {
   id: number;
@@ -22,11 +24,13 @@ type MovieProps = {
 export function Home() {
   const [genres, setGenres] = useState<GenreProps[]>([]);
   const [movies, setMovies] = useState<MovieProps[]>([]);
+  const [totalPages, setTotalPages] = useState(500); //default 500 because of api max validation
   const [searchParams, setSearchParams] = useSearchParams();
 
   const navigate = useNavigate();
 
   const filter = searchParams.get('filter');
+  const page = Number(searchParams.get('page'));
 
   useEffect(() => {
     async function getGenres() {
@@ -35,43 +39,74 @@ export function Home() {
         setGenres(response.data.genres);
       }
     }
+
     getGenres();
+
   }, []);
 
   useEffect(() => {
     async function getMovies() {
-      let response = await api.get("/movie/popular");
+      let response = await api.get("/movie/popular", {
+        params: { page: page === 0 ? page + 1 : page }
+      });
       if (response.status === 200) {
-        setMovies(response.data.results);
+        const { results, total_pages } = response.data;
+        setMovies(results);
+        if (total_pages < totalPages) {
+          setTotalPages(total_pages);
+        }
       }
     }
-    getMovies();
-  }, []);
-
-  useEffect(() => {
 
     async function getFilteredMovies() {
       let response = await api.get("/discover/movie", {
-        params: { with_genres: filter }
+        params: { with_genres: filter, page: page === 0 ? page + 1 : page }
       });
 
       if (response.status === 200) {
-        setMovies(response.data.results);
+        const { results, total_pages } = response.data;
+        setMovies(results);
+        if (total_pages < totalPages) {
+          setTotalPages(total_pages);
+        }
       }
     }
-    getFilteredMovies();
 
-  }, [searchParams]);
+    window.scrollTo(0, 0);
+    
+    if(filter) {
+      getFilteredMovies();
+      return;
+    }
+
+    getMovies();
+    return;
+
+  }, [filter, page, totalPages]);
 
   function filterMovies(genreId: number) {
     const genre = genreId.toString();
 
-    if (filter == genre) {
-      setSearchParams({})
+    const newParams = createSearchParams(searchParams);
+
+    if (filter === genre) {
+      newParams.delete('filter')
     } else {
-      setSearchParams({ filter: genreId.toString() })
+      newParams.set('filter', genre)
     }
+    
+    newParams.delete('page')
+    setSearchParams(newParams);
   }
+
+  function handlePageClick(event: { selected: number }) {
+    const newParams = createSearchParams(searchParams);
+
+    const page = event.selected + 1; //+1 because event is an index value and page param not
+
+    newParams.set('page', page.toString());
+    setSearchParams(newParams);
+  };
 
   return (
     <div className="home">
@@ -87,7 +122,7 @@ export function Home() {
                 {genres.map((genre) => (
                   <Button
                     key={genre.id}
-                    active={filter == genre.id.toString()}
+                    active={filter === genre.id.toString()}
                     onClick={() => filterMovies(genre.id)}
                   >
                     {genre.name}
@@ -111,6 +146,28 @@ export function Home() {
                 onClick={() => navigate(`movies/${movie.id}`)}
               />
             ))}
+          </div>
+
+          <div className="pagination">
+            <ReactPaginate
+              pageCount={totalPages}
+              pageRangeDisplayed={5}
+              marginPagesDisplayed={1}
+              onPageChange={handlePageClick}
+              forcePage={page === 0 ? 0 : page - 1} //check page because ReactPaginate works with index value and page param not
+              breakLabel="..."
+              nextLabel="&#10095;"
+              previousLabel="&#10094;"
+              containerClassName="pagination__buttons"
+              breakClassName="pagination__button"
+              pageClassName="pagination__button"
+              previousClassName="pagination__button"
+              nextClassName="pagination__button"
+              pageLinkClassName="pagination__button--link"
+              breakLinkClassName="pagination__button--link"
+              previousLinkClassName="pagination__button--link"
+              nextLinkClassName="pagination__button--link"
+            />
           </div>
         </div>
       </main>
